4f8b3a5ba72ef9af7fd9cabcebc89a3e9b1bcbea
tskoyo <jovansr@protonmail.com>
2025-03-04 21:39:44 +0100

    Code modifications

--- packages/plugin-notion/src/actions/users/listAllUsers.ts
+++ packages/plugin-notion/src/actions/users/listAllUsers.ts
@@ -8,18 +8,15 @@ import {
     State,
     elizaLogger,
 } from "@elizaos/core";
-import axios, { type AxiosRequestConfig } from "axios";
 
-const NOTION_API_KEY = "";
-const DEFAULT_TIMEOUT = 30000; // 30 seconds
-const BASE_URL = "https://api.notion.com";
-const VERSION = "/v1";
+import { sendNotionGetRequest, validateNotionApiKey } from "../action";
 
 export const listAllUsers: Action = {
     name: "LIST_ALL_USERS",
     description: "Lists all users within the workspace",
     similes: [],
     examples: [],
+    suppressInitialMessage: true,
     handler: async (
         runtime: IAgentRuntime,
         message: Memory,
@@ -27,45 +24,22 @@ export const listAllUsers: Action = {
         _options: { [key: string]: unknown },
         callback: HandlerCallback
     ) => {
-        try {
-            const config: AxiosRequestConfig = {
-                headers: {
-                    Authorization: `Bearer ${NOTION_API_KEY}`,
-                    "Notion-Version": "2022-06-28",
-                    "Content-Type": "application/json",
-                },
-                timeout: DEFAULT_TIMEOUT,
-            };
-            const url = BASE_URL + VERSION + "/users";
-            const response = await axios.get(url, config);
+        const apiKey = validateNotionApiKey();
 
-            const users = response.data.results
-                .map((user) => user.name)
-                .join(", ");
-
-            let currentState: State = state;
-            if (!currentState) {
-                currentState = (await runtime.composeState(message)) as State;
-            }
-            currentState = await runtime.updateRecentMessageState(currentState);
-
-            if (callback) {
-                callback({
-                    text: `Here are the users in your Notion workspace: ${users}`,
-                    content: users,
-                });
-            }
-            return true;
-        } catch (error) {
-            elizaLogger.error("Error in LIST_ALL_USERS handler:", error);
-
-            callback({
-                text: `Error fetching users: ${error.message}`,
-                content: { error: error.message },
-            });
-
-            return false;
+        if (!apiKey) {
+            elizaLogger.error("Invalid api key");
+            return;
         }
+
+        const response = await sendNotionGetRequest(apiKey, "/users");
+
+        const users = response.results.map((user) => user.name).join(", ");
+
+        callback({
+            text: `Here are the users in your Notion workspace: ${users}`,
+            content: users,
+        });
+        return true;
     },
     validate: async (runtime, _message) => {
         return !!runtime.getSetting("NOTION_API_KEY");
